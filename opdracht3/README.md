f8a1db0ea624ac425f239752be105b4a70da5450
hulsts002 <59019818+hulsts002@users.noreply.github.com>
2020-03-26 22:01:43 +0100

    Versie 2

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -1,3 +1,8 @@
 ## Vraag 19-03-2020
 Hoe kan ik, als ik op een article klik, andere informatie het beste laten tonen?
 Dus ipv van de info die je ziet als je de pagina laad, dat je ze vervangt met plot van de film (specifiek de article waar je op hebt geklikt).
+
+## Vraag 26-03-2020
+Hoe kan ik ervoor zorgen dat ik op Genre kan filteren?
+In opdracht twee heb ik de films een class geven en zo in css op display none kunnen zetten.
+Maar nu weet ik niet hoe ik onderscheid kan maken tussen de films en hun genres.
--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -3,109 +3,149 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
-
 const section = document.querySelector("section");
 var button = document.querySelector("button");
 //var article = document.querySelector("article");
 
-let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
+/* Doen als de pagina geladen is */
+window.onload = function() {
+   let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
 
+    request.responseType = 'json';
+    request.send();
 
-/* Gelijk laden als je pagina opent */
-request.onload = function () {
-    const movies = request.response;
-    showMovies(movies);
-}
-
-/* Laden wanneer je op knop klikt */
-//button.addEventListener('click', function () {
-//
-//    const movies = request.response;
-//    showMovies(movies);
-//
-//});
-
+    /* Doen als de server geantwoord heeft */
+    request.onload = function () {
+        const movies = request.response;
+        showMovies(movies);
+    }
+};
 
 function showMovies(jsonObj) {
     const movie = jsonObj;
 
     for (let i = 0; i < movie.length; i++) {
         const myArticle = document.createElement('article');
-        const Titel = document.createElement('h2');
-        const smallPlot = document.createElement('p');
-        const ReleaseDate = document.createElement('p');
-        const Genre = document.createElement('p');
-        const image = document.createElement('img');
 
-        Titel.textContent = movie[i].title;
-        smallPlot.textContent = movie[i].simple_plot;
-        ReleaseDate.textContent = 'Release Date: ' + movie[i].release_date;
-        Genre.textContent = 'Genre: ' + movie[i].genres;
-        image.src = movie[i].cover;
+        vulMetBasics(myArticle, movie[i]);
 
-        myArticle.setAttribute("id", 'Article' + i);
-        smallPlot.setAttribute("id", 'sP' + i);
-        ReleaseDate.setAttribute("id", 'rD' + i);
-        Genre.setAttribute("id", 'g' + i);
-
-        myArticle.appendChild(Titel);
-        myArticle.appendChild(smallPlot);
-        myArticle.appendChild(ReleaseDate);
-        myArticle.appendChild(Genre);
-        myArticle.appendChild(image);
+        /* bij het aanmaken van een film gelijk ook een eventListener toevoegen */
+        myArticle.addEventListener('click', function (event) {
+            vulFilmMetInfo(this);
+        });
 
         section.appendChild(myArticle);
     }
 }
 
 
-section.addEventListener('click', function (e) {
 
-        if (e.target.id == 'Article0') {
-            console.log("Article here");
-            const info = request.response;
-            showInfo(info);
+function vulFilmMetInfo(deFilm) {
+    /* het nummer van de film bepalen */
+    let filmID = deFilm.id;
+    let volgNummer = filmID.substring(7);
+
+
+    /* de info van alleen die film ophalen */
+    let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies'+volgNummer+'.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+
+    request.responseType = 'json';
+    request.send();
+
+    /* dit doen als de info van de film binnenkomst */
+    request.onload = function () {
+        const filmInfo = request.response;
+
+        /* als de film de class "details" heeft */
+        if(deFilm.classList.contains("details")){
+            /* de class "details" verwijderen */
+            deFilm.classList.remove("details");
+            /* de film vullen met de basics */
+            vulMetBasics(deFilm, filmInfo);
         }
+        /* als de film de class "details" niet heeft */
+        else {
+            /* de class "details" toevoegen */
+            deFilm.classList.add("details");
+            /* de film vullen met de details */
+            vulMetDetails(deFilm, filmInfo);
+        }
+    }
+}
 
-//    console.log("Article here");
-//    const info = request.response;
-//    showInfo(info);
+
+/* een film met de basics vullen */
+function vulMetBasics(myArticle, filmInfo) {
+    let Titel = document.createElement('h2');
+    let smallPlot = document.createElement('p');
+    let ReleaseDate = document.createElement('p');
+    let Genre = document.createElement('p');
+    let image = document.createElement('img');
+
+    Titel.textContent = filmInfo.title;
+    smallPlot.textContent = filmInfo.simple_plot;
+    ReleaseDate.textContent = 'Release Date: ' + filmInfo.release_date;
+    Genre.textContent = 'Genre: ' + filmInfo.genres;
+    image.src = filmInfo.cover;
+
+    myArticle.setAttribute("id", 'Article' + filmInfo.id);
+    smallPlot.setAttribute("id", 'sP' + filmInfo.id);
+    ReleaseDate.setAttribute("id", 'rD' + filmInfo.id);
+    Genre.setAttribute("id", 'g' + filmInfo.id);
+
+    /* article leeg maken */
+    myArticle.textContent = "";
+    /* article vullen */
+    myArticle.appendChild(Titel);
+    myArticle.appendChild(smallPlot);
+    myArticle.appendChild(ReleaseDate);
+    myArticle.appendChild(Genre);
+    myArticle.appendChild(image);
+}
+
+
+/* een film met details vullen */
+function vulMetDetails(myArticle, filmInfo){
+
+    let title = document.createElement('h2');
+    let plot = document.createElement('p');
+    title.textContent = filmInfo.title;
+    plot.textContent = filmInfo.plot;
+
+
+    /* article leeg maken */
+    myArticle.textContent = "";
+    /* article vullen */
+    myArticle.appendChild(title);
+    myArticle.appendChild(plot);
+}
+
+
+
+
+var darkModeContainer = document.querySelector(".darkMode");
+var filterContainer = document.querySelector(".filters");
+
+// Set Dark Mode
+darkModeContainer.addEventListener('change', function (event) {
+
+    if (event.target.checked) {
+        document.body.setAttribute("data-darkMode", "on");
+    } else {
+        document.body.setAttribute("data-darkMode", "off");
+    }
 
 });
 
+// Set Filters
+filterContainer.addEventListener('change', function (event) {
 
-function showInfo(jsonObj) {
-    const movie = jsonObj;
+    document.body.setAttribute("data-filter", event.target.id);
 
-    var img = document.querySelector('#Article0 img');
-    var sP = document.querySelector('#sP0');
-    var rD = document.querySelector('#rD0');
-    var g = document.querySelector('#g0');
 
-    var bigPlot = document.createElement('p');
-    bigPlot.textContent = movie[0].plot;
-
-    img.parentNode.removeChild(img);
-    rD.parentNode.removeChild(rD);
-    g.parentNode.removeChild(g);
-    sP.parentNode.replaceChild(bigPlot, sP);
-
-//
-//    for (let i = 0; i < movie.length; i++) {
-//
-//         var p = document.querySelector('h2');
-//
-//         const bigPlot = document.createElement('p');
-//
-//         bigPlot.textContent = movie[i].plot;
-//
-//         p.parentNode.replaceChild(bigPlot, p);
-//
-//    }
-
-}
+});
