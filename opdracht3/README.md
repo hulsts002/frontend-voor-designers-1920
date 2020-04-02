66d5399ee23c8ebfc7c0e598c9d76fcab646af9d
hulsts002 <59019818+hulsts002@users.noreply.github.com>
2020-04-02 19:05:48 +0200

    Versie 3

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -6,3 +6,38 @@ Dus ipv van de info die je ziet als je de pagina laad, dat je ze vervangt met pl
 Hoe kan ik ervoor zorgen dat ik op Genre kan filteren?
 In opdracht twee heb ik de films een class geven en zo in css op display none kunnen zetten.
 Maar nu weet ik niet hoe ik onderscheid kan maken tussen de films en hun genres.
+
+# Procesverslag
+
+Voor deze opdracht ga je een functionaliteit ontwerpen met externe data. De data is JSON die met een REST API van een externe bron wordt ingeladen met Javascript. Als de data geladen is moeten gebruikers je ontwerp op verschillende manieren kunnen bedienen. Verschillende states zijn vormgeven en worden op het juiste moment getoond.
+
+Link naar opdracht: https://hulsts002.github.io/frontend-voor-designers-1920/opdracht3/
+
+## Idee
+Door middel van opgehaalde informatie van films, de informatie tonen met als extra's een dark mode, filters, watchlist en aantal kolommen.
+Wanneer je hovered over een film, krijg je meer info te zien.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Schets.jpeg "Schets")
+
+## Versie 1
+Toen ik ben gaan werken met json, ben ik eigenlijk voor een ander design gegaan. Ik heb de films in 3 kolommen gezet met was basic informatie.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie1.1.png "Versie 1")
+
+## Versie 2
+In versie twee heb ik zitten spelen met de css, om het wat aantrekkelijker te maken. Tegelijkertijd ben ik gaan werken aan de Dark Mode, en hoe de kleuren hiermee zouden veranderen. Ook heb ik alvast een stukje voor de filters opgezet, maar deze zijn nog niet werkend.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie2.1.png "Versie 2.1")
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie2.2.png "Versie 2.2")
+
+## Versie 3
+Ik heb veel dingen toegevoegd sinds de laatste versie.
+Ten eerste als er nu op een film wordt geklikt, krijg je een gedetailleerde plot te zien.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie3.1.png "Versie 3.1")
+
+Daarna heb ik de functie toegevoegd om het aantal kolommen te veranderen. Wanneer je de browser resized, dan worden de kolommen vastgesteld door het systeem, en kan je deze als gebruiker niet meer aanpassen.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie3.2.png "Versie 3.2")
+
+Toen ben ik bezig geweest met de filters. Wanneer er op één van de genre's wordt geklikt, krijg je alleen de films te zien die hierin voldoen.
+Hij werkt nog niet optimaal, hij pakt op dit moment alleen nog de eerste genre (van de genre's die je kan zien bij de film).
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie3.3.png "Versie 3.3")
+
+Als laatst heb ik een info icon toegevoegd, wanneer je hier met je muis over heen hovered dan krijg je een blok tekst te zien met de hotkeys die gebruikt kunnen worden.
+![alt text](https://github.com/hulsts002/frontend-voor-designers-1920/blob/master/opdracht3/img/Readme/Versie3.4.png "Versie 3.4")
--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -5,8 +5,6 @@
 /*eslint 'no-console': 0*/
 const section = document.querySelector("section");
 var button = document.querySelector("button");
-//var article = document.querySelector("article");
-
 
 /* Doen als de pagina geladen is */
 window.onload = function () {
@@ -22,6 +20,7 @@ window.onload = function () {
         const movies = request.response;
         showMovies(movies);
     }
+
 };
 
 function showMovies(jsonObj) {
@@ -41,13 +40,10 @@ function showMovies(jsonObj) {
     }
 }
 
-
-
 function vulFilmMetInfo(deFilm) {
     /* het nummer van de film bepalen */
     let filmID = deFilm.id;
     let volgNummer = filmID.substring(7);
-
 
     /* de info van alleen die film ophalen */
     let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies' + volgNummer + '.json';
@@ -78,13 +74,11 @@ function vulFilmMetInfo(deFilm) {
     }
 }
 
-
 /* een film met de basics vullen */
 function vulMetBasics(myArticle, filmInfo) {
     let Titel = document.createElement('h2');
     let smallPlot = document.createElement('p');
     let ReleaseDateGenre = document.createElement('p');
-    //    let Genre = document.createElement('p');
     let image = document.createElement('img');
 
     Titel.textContent = filmInfo.title;
@@ -92,21 +86,35 @@ function vulMetBasics(myArticle, filmInfo) {
     image.src = filmInfo.cover;
     var date = new Date(filmInfo.release_date);
     ReleaseDateGenre.textContent = date.getFullYear() + " | " + filmInfo.genres.join(' / ');
-    //    Genre.textContent = 'Genre: ' + filmInfo.genres;
+
+
+    //class toevoegen voor filters?
+    if (filmInfo.genres.includes("Horror")) {
+        myArticle.setAttribute("class", 'horror');
+        console.log("horror");
+    } else if (filmInfo.genres.includes("Crime")) {
+        myArticle.setAttribute("class", 'crime');
+        console.log("crime");
+    } else if (filmInfo.genres.includes("Drama")) {
+        myArticle.setAttribute("class", 'drama');
+        console.log("drama");
+    } else if (filmInfo.genres.includes("Action")) {
+        myArticle.setAttribute("class", 'action');
+        console.log("action");
+    }
+
 
     myArticle.setAttribute("id", 'Article' + filmInfo.id);
     smallPlot.setAttribute("id", 'sP' + filmInfo.id);
     ReleaseDateGenre.setAttribute("id", 'rDg' + filmInfo.id);
-    //    Genre.setAttribute("id", 'g' + filmInfo.id);
 
     /* article leeg maken */
     myArticle.textContent = "";
     /* article vullen */
     myArticle.appendChild(Titel);
+    myArticle.appendChild(ReleaseDateGenre);
     myArticle.appendChild(smallPlot);
     myArticle.appendChild(image);
-    myArticle.appendChild(ReleaseDateGenre);
-    //    myArticle.appendChild(Genre);
 }
 
 
@@ -127,9 +135,8 @@ function vulMetDetails(myArticle, filmInfo) {
 }
 
 
-
-
 var darkModeContainer = document.querySelector(".darkMode");
+var columnsContainer = document.querySelector(".columns");
 var filterContainer = document.querySelector(".filters");
 
 // Set Dark Mode
@@ -143,6 +150,13 @@ darkModeContainer.addEventListener('change', function (event) {
 
 });
 
+// Set amount of Columns
+columnsContainer.addEventListener('change', function (event) {
+
+    document.body.setAttribute("data-columns", event.target.id);
+
+});
+
 // Set Filters
 filterContainer.addEventListener('change', function (event) {
 
@@ -153,20 +167,46 @@ filterContainer.addEventListener('change', function (event) {
 
 // Set Columns and Dark Mode with Keyboard Input
 document.body.addEventListener('keydown', function (event) {
-
     if (event.keyCode == 76) {
         document.body.setAttribute("data-darkMode", "off");
         var inputOff = document.querySelector("#checkbox");
         inputOff.checked = false;
-        inputOff.focus();
     } else if (event.keyCode == 68) {
         document.body.setAttribute("data-darkMode", "on");
         var inputOn = document.querySelector("#checkbox");
         inputOn.checked = true;
-        inputOn.focus();
+    } else if (event.keyCode == 50) {
+        document.body.setAttribute("data-columns", "twee");
+        var input2 = document.querySelector("#twee");
+        input2.checked = true;
+    } else if (event.keyCode == 51) {
+        document.body.setAttribute("data-columns", "drie");
+        var input3 = document.querySelector("#drie");
+        input3.checked = true;
+    } else if (event.keyCode == 52) {
+        document.body.setAttribute("data-columns", "vier");
+        var input4 = document.querySelector("#vier");
+        input4.checked = true;
     }
-
-
 });
 
 
+/* On resize of window, you can't change the amount of columns */
+var w = window.innerWidth;
+
+window.addEventListener('resize', function (event) {
+
+    var inputTwo = document.querySelector("#twee");
+    var inputThree = document.querySelector("#drie");
+    var inputFour = document.querySelector("#vier");
+
+    if (window.innerWidth < 1040) {
+        inputTwo.disabled = true;
+        inputThree.disabled = true;
+        inputFour.disabled = true;
+    } else {
+        inputTwo.disabled = false;
+        inputThree.disabled = false;
+        inputFour.disabled = false;
+    };
+});
